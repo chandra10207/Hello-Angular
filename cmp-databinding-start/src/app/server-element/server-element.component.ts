0a24dc3cdcea0f46dea0a1e2ef2c3789c844e4ac
Chandra Shekhar <chandra.pandey@winning.com.au>
2022-12-08 10:55:37 +1100

    View encapsulation, css encapsulation, css rules

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //Default Emulated,  None, Naitive
 })
 export class ServerElementComponent implements OnInit {
 
